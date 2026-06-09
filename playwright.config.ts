256ab1e51dfd3dbf4252588b497d009661f57d69
axi92 <axi92@users.noreply.github.com>
2026-06-09 13:47:46 +0200

    test: add mobile browser to test

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,37 +38,18 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-      workers: 1,
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 7'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 15'] },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
